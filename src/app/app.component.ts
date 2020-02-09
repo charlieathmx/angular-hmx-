dc83b5f7717bc0e6988661fd24b5adb154598513
DadBot <60844368+charlieathmx@users.noreply.github.com>
2020-02-09 11:04:37 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 }
+import { render } from '@testing-library/angular';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('should render the component', async () => {
+    const { getByText } = await render(AppComponent);
+    expect(getByText('Welcome'));
+  });
+});
